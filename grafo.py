ed9e50d65608c447212fffe743847753c9452ff3
alejo190404 <alejo190404@gmail.com>
2024-03-14 22:33:58 -0500

    Pruebas finales

--- grafo.py
+++ grafo.py
@@ -1,25 +1,75 @@
 class Grafo:
+    
+    def __init__(self, laberinto):
+        self.lista_adyacencia = self.laberinto_a_grafo(laberinto)
+        self.laberinto = laberinto  # Guardamos el laberinto para calcular la heurística en A*
 
-    def __init__(self, lista_adyacencia):
-        self.lista_adyacencia = lista_adyacencia
+    def laberinto_a_grafo(self, laberinto):
+        grafo = {}
+        n = len(laberinto)  # Asumiendo que el laberinto es cuadrado
+        for i in range(n):
+            for j in range(n):
+                if laberinto[i][j] != 1:  # Si no es una pared
+                    vecinos = []
+                    if i > 0 and laberinto[i-1][j] != 1:
+                        vecinos.append((i-1, j))  # Arriba
+                    if i < n-1 and laberinto[i+1][j] != 1:
+                        vecinos.append((i+1, j))  # Abajo
+                    if j > 0 and laberinto[i][j-1] != 1:
+                        vecinos.append((i, j-1))  # Izquierda
+                    if j < n-1 and laberinto[i][j+1] != 1:
+                        vecinos.append((i, j+1))  # Derecha
+                    grafo[(i, j)] = vecinos
+        return grafo
 
     def obtener_vecinos(self, v):
-        return self.lista_adyacencia[v]
+        return self.lista_adyacencia.get(v, [])
 
-    # funcion heuristica
-    def h(self, n):
-        #inserte su codigo aqui
-        return H[n] # puede retornar una lista con el calculo de la heuristica para cada estado
+    def h(self, nodo, nodo_final):
+        # Usando la distancia Manhattan como heurística
+        return abs(nodo[0] - nodo_final[0]) + abs(nodo[1] - nodo_final[1])
 
     def primero_profundidad(self, nodo_inicio, nodo_final):
-       #inserte si codigo aqui
+        visitado = set()
+        pila = [(nodo_inicio, [nodo_inicio])]
+
+        while pila:
+            (vertice, camino) = pila.pop()
+            if vertice not in visitado:
+                if vertice == nodo_final:
+                    return camino
+                visitado.add(vertice)
+                for vecino in self.obtener_vecinos(vertice):
+                    pila.append((vecino, camino + [vecino]))
         return None
-        
+
     def primero_anchura(self, nodo_inicio, nodo_final):
-       #inserte si codigo aqui
+        visitado = set()
+        cola = [(nodo_inicio, [nodo_inicio])]
+
+        while cola:
+            (vertice, camino) = cola.pop(0)
+            if vertice not in visitado:
+                if vertice == nodo_final:
+                    return camino
+                visitado.add(vertice)
+                for vecino in self.obtener_vecinos(vertice):
+                    cola.append((vecino, camino + [vecino]))
         return None
-    
+
     def a_estrella(self, nodo_inicio, nodo_final):
-       #inserte si codigo aqui
+        import heapq
+        cola_prioridad = [(self.h(nodo_inicio, nodo_final), 0, nodo_inicio, [nodo_inicio])]
+        visitado = set()
+
+        while cola_prioridad:
+            (costo_estimado, costo, vertice, camino) = heapq.heappop(cola_prioridad)
+            if vertice not in visitado:
+                if vertice == nodo_final:
+                    return camino
+                visitado.add(vertice)
+                for vecino in self.obtener_vecinos(vertice):
+                    if vecino not in visitado:
+                        nuevo_costo = costo + 1  # Consideramos costo uniforme para cada movimiento
+                        heapq.heappush(cola_prioridad, (nuevo_costo + self.h(vecino, nodo_final), nuevo_costo, vecino, camino + [vecino]))
         return None
-    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #importar librerias
 import sys
+from grafo import Grafo
 
 #Definir funciones
 #Imprimir matriz
@@ -10,7 +11,20 @@ def imprimirMatriz(matriz):
             print(matriz[i][j], end="\t")
         print()
 
-#Generar camino de forma recursiva
+def cargar_laberinto(ruta_archivo):
+    laberinto = []
+    with open(ruta_archivo, 'r') as archivo:
+        for linea in archivo:
+            # Convierte cada línea en una lista de enteros
+            fila = [int(x) for x in linea.strip().replace('[','').replace(']','').split(',')]
+            laberinto.append(fila)
+    return laberinto
+
+def procesar_coordenadas(coordenadas_str):
+    """Convierte una cadena de coordenadas '(x,y)' en una tupla de enteros (x, y)."""
+    x, y = coordenadas_str.strip("()").split(",")
+    return int(x), int(y)
+
 def camino(coordenada_actual, visitada, laberinto, tam, lista_vertices, lista_nodos_adyacentes):
     if (visitada[coordenada_actual[0]][coordenada_actual[1]] == 0 and not(tieneCaminosAdyacentes(coordenada_actual, visitada, laberinto, tam))): #Caso base: No es visitado y no tiene caminos adyacentes no visitados
         return
@@ -46,34 +60,37 @@ def tieneCaminosAdyacentes(coordenada_actual, visitada, laberinto, tam):
 if (len(sys.argv) != 5):
     print("Usage: main.py <archivo de matriz> <dimension N de la matriz> <coordenada de salida (x,y)> <coordenada de meta (x,y)>")
 
-n = int(sys.argv[2])
+
 
 #Cargar archivo de entrada y asignarlo como matriz
-with open(sys.argv[1]) as f:
-    laberinto = f.readlines()
 
-for i in range(len(laberinto)):
-    laberinto[i] = laberinto[i].replace("[", "")
-    laberinto[i] = laberinto[i].replace(" ", "")
-    laberinto[i] = laberinto[i].replace("]", "")
-    laberinto[i] = laberinto[i].split(",")
-    for j in range(len(laberinto[i])):
-        laberinto[i][j] = int(laberinto[i][j])
+n = int(sys.argv[2])
+ruta_archivo = sys.argv[1]
 
+# Cargar el laberinto desde el archivo
+laberinto = cargar_laberinto(ruta_archivo)
+print("Laberinto: ")
 imprimirMatriz(laberinto)
 
 #Transformar a grafo
+grafo = Grafo(laberinto)
+inicio_str = sys.argv[3]
+fin_str = sys.argv[4]
+inicio = procesar_coordenadas(inicio_str)
+fin = procesar_coordenadas(fin_str)
+
 lista_vertices = {}
 lista_nodos_adyacentes = {}
 
 #Matriz para saber si ya se visitó la coordenada
 visitada = [[0 for j in range(n)] for i in range(n)]
-imprimirMatriz(visitada)
 
-#Determinar qué "coordenadas" son vértices
-# for i in range(n):
-#     for j in range(n):
-#         if (laberinto[i][j] != 1):
-#             camino([i, j], visitada, laberinto, n, lista_vertices, lista_nodos_adyacentes)
 
 print(tieneCaminosAdyacentes([1, 1], visitada, laberinto, n))
+#Generar camino
+caminoProfundidad = grafo.primero_profundidad(inicio, fin)
+print("Camino encontrado:", caminoProfundidad)
+caminoAnchura = grafo.primero_anchura(inicio, fin)
+print("Camino encontrado:", caminoAnchura)
+caminoAEstrella = grafo.a_estrella(inicio, fin)
+print("Camino encontrado:", caminoAnchura)
